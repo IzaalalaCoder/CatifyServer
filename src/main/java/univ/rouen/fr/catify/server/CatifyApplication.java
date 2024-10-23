f7c85b5c86d476693ae33a6cea8f36b229bce4f7
Izaalala <86750551+IzaalalaCoder@users.noreply.github.com>
2024-10-23 08:55:23 +0200

    feat(core): initialize project structure

---
+++ src/main/java/univ/rouen/fr/catify/server/CatifyApplication.java
@@ -0,0 +1,13 @@
+package univ.rouen.fr.catify.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/univ/rouen/fr/catify/server/CatifyApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.rouen.fr.catify.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
